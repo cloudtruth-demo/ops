19c00e3b9ee12c7892261c9fa12600bfdce995ba
Matt Conway <matt@conwaysplace.com>
2020-09-17 08:56:02 -0400

    sample data bucket

--- recipes/service-demo1.tf
+++ recipes/service-demo1.tf
@@ -1,9 +1,9 @@
 locals {
   demo1_env = {
     AWS_REGION = var.region
-    SVC_ENV  = var.atmos_env
-    SVC_NAME = "$${name}"
-    SVC_PORT = "$${port}"
+    SVC_ENV    = var.atmos_env
+    SVC_NAME   = "$${name}"
+    SVC_PORT   = "$${port}"
   }
 
   demo1_svc_env = join(",\n", formatlist("{ \"name\" : \"%s\", \"value\" : \"%s\" }", keys(local.demo1_env), values(local.demo1_env)))
@@ -24,7 +24,7 @@ module "service-demo1-alb" {
   vpc_id        = module.vpc.vpc_id
   logs_bucket   = aws_s3_bucket.logs.bucket
 
-  destination_port      = module.service-demo1.port
+  destination_port = module.service-demo1.port
 
   destination_security_group = module.service-demo1.security_group_id
   alb_certificate_arn        = module.wildcart-cert.certificate_arn
@@ -43,13 +43,13 @@ module "service-demo1" {
 
   // The default security groups allow outbound to internet which is required for
   // pulling docker image from ECR
-  security_groups         = module.vpc.security_group_ids
+  security_groups = module.vpc.security_group_ids
 
   name            = "demo1"
   ecs_cluster_arn = aws_ecs_cluster.services.arn
 
-  create_repository = 1
-  integrate_with_lb = 1
+  create_repository   = 1
+  integrate_with_lb   = 1
   alb_target_group_id = module.service-demo1-alb.lb_target_group_id
 
   port = 8000
@@ -88,4 +88,28 @@ module "service-demo1" {
     ]
 TMPL
 
+}
+
+
+// Bucket with some extra sample data for cloudtruth demo
+resource "aws_s3_bucket" "sample-data" {
+  bucket        = "${var.global_name_prefix}sample-data"
+  force_destroy = var.force_destroy_buckets
+
+  tags = {
+    Env    = var.atmos_env
+    Source = "atmos"
+  }
+}
+
+resource "aws_s3_bucket_object" "sample-json" {
+  bucket = aws_s3_bucket.sample-data.bucket
+  key = "data/sample.json"
+  source = "../templates/sample.json"
+}
+
+resource "aws_s3_bucket_object" "sample-yml" {
+  bucket = aws_s3_bucket.sample-data.bucket
+  key = "data/sample.yml"
+  source = "../templates/sample.yml"
 }
