e7520744457b4e0b4b2890ae8985e4183889864e
Matt Conway <matt@conwaysplace.com>
2020-09-30 13:51:16 -0400

    limit s3 access

--- recipes/cloudtruth-access.tf
+++ recipes/cloudtruth-access.tf
@@ -4,4 +4,10 @@ module "grant-cloudtruth-access" {
   external_id = ""
   account_ids = ["811566399652", "760722967169"]
   services_enabled = ["s3", "ssm"]
+  s3_resources     = [
+    "arn:aws:s3:::ctdemo-production-sample-data",
+    "arn:aws:s3:::ctdemo-production-sample-data/*",
+    "arn:aws:s3:::ctdemo-ops-terraform-state",
+    "arn:aws:s3:::ctdemo-ops-terraform-state/*"
+  ]
 }
