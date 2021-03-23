5ada37a93299f673a9140035880b47f3759cd41c
Matt Conway <matt@conwaysplace.com>
2021-03-23 11:05:39 -0400

    enable secretsmanager access and restrict access to sample* for ssm/secrets

--- recipes/cloudtruth-access.tf
+++ recipes/cloudtruth-access.tf
@@ -3,7 +3,7 @@ module "grant-cloudtruth-access" {
   role_name = "cloudtruth"
   external_id = "07e76c9c-cf3a-41ce-87bc-6d1e5a3a0af6,edd14b2d-5b95-4f69-b3c5-d78447d86926,c47fd6ce-0953-4b39-8c99-96afea2624ee"
   account_ids = ["811566399652", "760722967169"]
-  services_enabled = ["s3", "ssm"]
+  services_enabled = ["s3", "ssm", "secrets"]
   s3_resources     = [
     "arn:aws:s3:::ctdemo-production-sample-data",
     "arn:aws:s3:::ctdemo-production-sample-data/*",
@@ -14,4 +14,13 @@ module "grant-cloudtruth-access" {
     "arn:aws:s3:::ctdemo-production-terraform-state",
     "arn:aws:s3:::ctdemo-production-terraform-state/*"
   ]
+
+  ssm_resources = [
+    "arn:aws:ssm:${var.region}:${var.account_ids[var.atmos_env]}:parameter/sample*"
+  ]
+
+  secrets_resources = [
+    "arn:aws:secretsmanager:${var.region}:${var.account_ids[var.atmos_env]}:secret:sample*"
+  ]
 }
+
